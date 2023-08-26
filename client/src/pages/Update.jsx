bfa55527777a05a002a4ccc38011703ff6c2459a
Cosmin Andrei Vasilescu <c.vasilescu-1@student.tudelft.nl>
2023-08-27 00:09:28 +0300

    improve the update function to contain prev data

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
-import { useLoaderData, useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
-const Update = () => {
-    const [book, setBook] = useState({
-        title: "",
-        desc: "",
-        price: null,
-        cover: ""
-    })
-
+const Update = (props) => {
     const navigate = useNavigate()
     const location = useLocation()
 
     const bookId = location.pathname.split("/")[2]
-
-    console.log(bookId)
+    const bookData = location.state
+    
+    const [book, setBook] = useState({
+        title: bookData.title,
+        desc: bookData.desc,
+        price: bookData.price,
+        cover: bookData.cover
+    })
 
     const handleChange = (e) => {
         setBook(prev => ({ ...prev, [e.target.name]: e.target.value }))
     }
 
     const handleClick = async (e) => {
-        console.log(book)
         e.preventDefault()
         try{
             await axios.put("http://localhost:8800/books/" + bookId, book)
@@ -36,10 +34,10 @@ const Update = () => {
     return (
         <div className="form">
             <h1>Update the book</h1>
-            <input type="text" placeholder='title' onChange={handleChange} name="title" />
-            <input type="text" placeholder='desc' onChange={handleChange} name="desc" />
-            <input type="number" placeholder='price' onChange={handleChange} name="price" />
-            <input type="text" placeholder='cover' onChange={handleChange} name="cover" />
+            <input type="text" placeholder='title' onChange={handleChange} name="title" defaultValue={bookData.title} />
+            <input type="text" placeholder='desc' onChange={handleChange} name="desc" defaultValue={bookData.desc}/>
+            <input type="number" placeholder='price' onChange={handleChange} name="price" defaultValue={bookData.price} />
+            <input type="text" placeholder='cover' onChange={handleChange} name="cover" defaultValue={bookData.cover} />
             <button className='formButton' onClick={handleClick}>Update</button>
         </div>
     )
